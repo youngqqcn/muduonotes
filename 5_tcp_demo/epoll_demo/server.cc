7ac6df9a8bb98eebf1af2f0c8ac92617b268f1d2
yqq <youngqqcn@163.com>
2020-12-20 17:43:20 +0800

    mdify  per-connection-per-thread to  add epoll

--- 5_tcp_demo/epoll_demo/server.cc
+++ 5_tcp_demo/epoll_demo/server.cc
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/epoll.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
@@ -8,6 +7,9 @@
 #include <vector>
 #include <thread>
 #include <arpa/inet.h>
+
+
+ #include <sys/epoll.h>  // epoll
 
 using namespace std;
 
@@ -47,45 +49,111 @@ int main(int argc, char const *argv[])
         return 0;
     }
 
+
+    int epfd = epoll_create(1000);
+    if(epfd < 0)
+    {
+        printf("epoll create error\n");
+        return 0;
+    }
+
+    struct epoll_event ev;
+    ev.data.fd = fd;
+
+    // EPOLLET 边缘触发, 一旦有数据到达就触发,只触发一次; 
+    // EPOLLLT  水平触发 , 只要有数据未被处理, 就一直通知
+    ev.events = EPOLLIN | EPOLLET; 
+
+    if(0 > epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev))
+    {
+        printf("epoll_ctl error\n");
+        return 0;
+    }
+
+    
+
+
+
     // int clientFds[];
     vector<int> vctClientFds;
     vector<thread> vctThreads;
 
-    sockaddr_in cliAddr;
-    socklen_t cliSckkLen = sizeof(cliAddr);
-    memset(&cliAddr, 0, sizeof(sizeof(cliAddr)));
+    char recvBuf[1024] = {0} ;
     while(1)
     {
-        int cliFd = ::accept(fd, (struct sockaddr* )(&cliAddr), &cliSckkLen);
-        if(cliFd < 0)
+        struct epoll_event events[1000];
+        int ret = epoll_wait(epfd, events, sizeof(events)/sizeof(events[0]) , -1);
+        if(0 > ret)
         {
-            perror("error accept\n");
-            continue;
+            printf("epoll_wait error\n");
+            return 0;
+        }
+        else if(0 == ret )
+        {
+            printf("timeout\n");
+            return 0;
         }
 
-        vctClientFds.push_back(cliFd);
+        // 便利event
+        for(int i = 0; i < ret; i++)
+        {
+            struct epoll_event evt;
+            memcpy(&evt,  &events[i], sizeof(epoll_event));
 
-        vctThreads.emplace_back([cliFd, cliAddr](){
-            char buf[1024] = {0} ;
-            while(1)
+            if( (evt.events & EPOLLERR) || (evt.events & EPOLLHUP) || !(evt.events & EPOLLIN)  )
             {
-                memset(buf, 0, sizeof(buf));
-                int ret = recv(cliFd, buf, sizeof(buf), 0);
+                printf("epoll error");
+                close(evt.data.fd);
+                return 0;
+            }
+
+            // 新的连接
+            if(evt.data.fd == fd) 
+            {
+                struct sockaddr_in cliAddr;
+                socklen_t cliSckkLen = sizeof(cliAddr);
+                int cliFd = ::accept(fd, (struct sockaddr* )(&cliAddr), &cliSckkLen);
+                if(cliFd < 0)
+                {
+                    perror("error accept\n");
+                    continue;
+                }
+                struct epoll_event event;
+                event.data.fd = cliFd;
+                event.events = EPOLLIN | EPOLLET;
+                epoll_ctl(epfd, EPOLL_CTL_ADD, cliFd, &event);
+                vctClientFds.push_back(cliFd);
+
+                // printf("accepted connection from %s, port : %d\n", inet_ntoa( cliAddr.sin_addr), ntohl(cliAddr.sin_port));
+                printf("accepted connection from %s, port : %d\n", inet_ntoa( cliAddr.sin_addr), ntohs(cliAddr.sin_port));
+            }
+            else if( evt.events & EPOLLIN ) // 数据
+            {
+                memset(recvBuf, 0, sizeof(recvBuf));
+                int clifd = evt.data.fd;
+                int ret = recv(clifd, recvBuf, sizeof(recvBuf), 0);
                 if(ret < 0)
                 {
-                    printf(" recv error , socket fd %d\n", cliFd);
-                    close(cliFd);
-                    return;
+                    printf(" recv error , socket fd %d\n", clifd);
+                    close(clifd);
                 }
                 else if(0 == ret)
                 {
-                    printf(" client : %s close socket\n ", inet_ntoa( cliAddr.sin_addr));
-                    close(cliFd);
-                    return;
+                    printf("client close socket fd\n");
+                    close(clifd);
                 }
-                printf("%s %d bytes data, date is : %s \n",  inet_ntoa( cliAddr.sin_addr), ret , buf);
+                else
+                {
+                    printf("client socket fd: %d , send %d bytes data, date is : %s \n", evt.data.fd , ret , recvBuf);
+                }
+                
             }
-        });
+
+        }
+
+
+
+        
     }
 
     return 0;
